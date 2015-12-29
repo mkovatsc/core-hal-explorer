dd665bb768b8291d4b3a06095906e5f851215a47
Yassin N. Hassan <hassany@student.ethz.ch>
2015-12-29 23:30:24 +0100

    first commit

---
+++ gulpfile.js
@@ -0,0 +1,190 @@
+'use strict';
+// generated on 2015-12-28 using generator-gulp-bootstrap3 0.4.4
+
+// Load plugins
+var gulp = require('gulp'),
+    sass = require('gulp-ruby-sass'),
+    autoprefixer = require('gulp-autoprefixer'),
+    minifycss = require('gulp-minify-css'),
+    uglify = require('gulp-uglify'),
+    imagemin = require('gulp-imagemin'),
+    rename = require('gulp-rename'),
+    del = require('del'),
+    concat = require('gulp-concat'),
+    notify = require('gulp-notify'),
+    cache = require('gulp-cache'),
+    include = require('gulp-include'),
+    ejs = require('gulp-ejs'),
+    gutil = require('gulp-util'),
+    revall = require('gulp-rev-all'),
+    livereload = require('gulp-livereload'),
+    gulpif = require('gulp-if'),
+    flatten = require('gulp-flatten'),
+    iconfont = require('gulp-iconfont'),
+    iconfontCss = require('gulp-iconfont-css'),
+    sprity = require('sprity');
+    
+// Define paths
+var paths = {  
+  scripts:   ['./src/js/*.js'],
+  styles:    ['./src/css/*.{scss,sass,css}'],
+  images:    ['./src/images/**', '!./src/images/sprite/**', '!./src/images/sprite/'],
+  templates: ['./src/templates/*.ejs']
+};
+
+// CSS
+gulp.task('css', function() {
+  return sass(paths.styles, {
+    style: 'expanded',
+    loadPath: [
+      process.cwd() + '/src/css/partials',
+      process.cwd() + '/src/vendor'
+    ]
+  })
+  .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'))
+  .pipe(gulp.dest('dist/assets/css'))
+  .pipe(rename({suffix: '.min'}))
+  .pipe(minifycss())
+  .pipe(gulp.dest('dist/assets/css'))
+  .pipe(notify({ message: 'CSS task complete' }));
+});
+
+// Javascript
+gulp.task('js', function() {
+  return gulp.src(paths.scripts)
+    .pipe(include().on('error', gutil.log))
+    .pipe(gulp.dest('dist/assets/js'))
+    .pipe(rename({ suffix: '.min' }))
+    .pipe(uglify().on('error', gutil.log))
+    .pipe(gulp.dest('dist/assets/js'))
+    .pipe(notify({ message: 'JS task complete' }));
+});
+
+// Sprite generation
+gulp.task('sprites', function(cb) {
+  return sprity.src({
+    src: './src/images/sprite/*.png',
+    style: './_sprite.sass',
+    processor: 'sass',
+    'style-type': 'sass',
+    prefix: 'sprite',
+    dimension: [{
+      ratio: 1, dpi: 72
+    }, {
+      ratio: 2, dpi: 192
+    }],
+  }).on('error', function(err) {
+    gutil.log(err.toString());
+    cb();
+  })
+  .pipe(gulpif('*.png', gulp.dest('./src/images/'), gulp.dest('./src/css/partials/')))
+});
+
+// Optimize images
+gulp.task('images', function() {
+  return gulp.src(paths.images)
+    .pipe(cache(imagemin({ optimizationLevel: 5, progressive: true, interlaced: true })))
+    .pipe(gulp.dest('dist/assets/images'))
+    .pipe(notify({ message: 'Images task complete' }));
+});
+
+// Templates
+gulp.task('templates', function() {
+  return gulp.src(paths.templates)
+    .pipe(ejs().on('error', gutil.log))
+    .pipe(gulp.dest('dist'))
+    .pipe(notify({ message: 'Templates task complete' }));
+});
+
+// Clean up
+gulp.task('clean', function() {
+  return del(['dist/assets/css', 'dist/assets/js', 'dist/assets/images', 'dist/assets/fonts', 'dist/*.html']);
+});
+
+// Rev all files
+gulp.task('rev', function () {
+  gulp.src('dist/**')
+    .pipe(revall({ ignore: [/^\/favicon.ico$/g, '.html'] }))
+    .pipe(gulp.dest('rev'));
+});
+
+// Copy fonts
+gulp.task('fonts', function() {
+  gulp.src('src/**/*.{eot,svg,ttf,woff,woff2}')
+    .pipe(flatten())
+    .pipe(gulp.dest('dist/assets/fonts'));
+});
+
+// Create icon font
+var fontName = 'webicons';
+gulp.task('iconfont', function(){
+  gulp.src(['./src/images/icons/*.svg'])
+    .pipe(iconfontCss({
+      fontName: fontName,
+      targetPath: '../css/partials/_icons.scss',
+      fontPath: '../fonts/'
+    }))
+    .pipe(iconfont({
+      fontName: fontName
+     }))
+    .pipe(gulp.dest('./src/fonts'));
+});
+
+// Default task
+gulp.task('default', ['clean', 'sprites', 'iconfont'], function() {
+  gulp.start('css', 'js', 'images', 'templates', 'fonts');
+});
+
+// Setup connect server
+gulp.task('connect', function() {
+  var connect = require('connect');
+  var app = connect()
+      .use(require('connect-livereload')({ port: 35729 }))
+      .use(require('serve-static')('dist'))
+      .use(require('serve-index')('dist'));
+        
+  require('http').createServer(app)
+    .listen(9000)
+    .on('listening', function() {
+      console.log('Started connect web server on http://localhost:9000');
+    });
+});
+
+// Serve
+gulp.task('serve', ['connect'], function() {
+  require('opn')('http://localhost:9000');
+});
+
+// Watch
+gulp.task('watch', ['connect', 'serve'], function() {
+
+  // Watch SASS files
+  gulp.watch('src/css/**/*.sass', ['css']);
+  
+  // Watch JS files
+  gulp.watch('src/js/**/*.js', ['js']);
+
+  // Watch image files
+  gulp.watch(paths.images, ['images']);
+
+  // Watch iconfonts files
+  gulp.watch('./src/images/icons/*.svg', ['iconfont', 'fonts']);
+  
+  // Watch sprite folder
+  gulp.watch('./src/images/sprite/*.png', ['sprites']);
+
+  // Watch template files
+  gulp.watch('./src/templates/**/*.ejs', ['templates']);
+  
+  // Watch for fonts
+  gulp.watch('./src/**/*.{eot,svg,ttf,woff,woff2}', ['fonts']);
+  
+  // Create LiveReload server
+  livereload({ start: true });
+
+  // Watch any files in assets folder reload on change
+  gulp.watch(['dist/assets/**', 'dist/*.html']).on('change', function(file) {
+    livereload.changed(file.path);
+  });
+
+});
---
+++ src/js/app.js
@@ -0,0 +1,249 @@
+//= require ../vendor/jquery/dist/jquery.js
+//= require ../vendor/lodash/lodash.js
+//= require ../vendor/d3/d3.js
+//= require ../vendor/jquery-jsonview/dist/jquery.jsonview.js
+//= require ../vendor/bootstrap-sass-official/assets/javascripts/bootstrap.js
+//= require partials/main.js
+$(window).resize(function() {
+    update();
+
+});
+var color = d3.scale.category10();
+$(".run").on("click", function() { 
+	for(var k in nodes){
+		delete nodes[k];
+	}
+	links = [];
+	update();
+    process($("input").val());
+});
+//http://stackoverflow.com/questions/470832/getting-an-absolute-url-from-a-relative-one-ie6-issue
+function resolve(url, base_url) {
+  var doc      = document
+    , old_base = doc.getElementsByTagName('base')[0]
+    , old_href = old_base && old_base.href
+    , doc_head = doc.head || doc.getElementsByTagName('head')[0]
+    , our_base = old_base || doc_head.appendChild(doc.createElement('base'))
+    , resolver = doc.createElement('a')
+    , resolved_url
+    ;
+  our_base.href = base_url;
+  resolver.href = url;
+  resolved_url  = resolver.href; // browser magic at work here
+
+  if (old_base) old_base.href = old_href;
+  else doc_head.removeChild(our_base);
+  return resolved_url;
+}
+var nodes = {};
+function process(url) {
+	if(url in nodes)
+		return;
+    $.get(url, function(data) {
+    	if(data[0]=='<'){
+	        _.map(_.filter(data.split(','), function(el) {
+	            return el.length > 0;
+	        }), function(el) {
+	            console.log(el);
+	            var val = el.substring(1, el.indexOf('>'));
+	    		var uri = resolve(val, url);
+	            if(el.indexOf('ct=6')>0){
+				    links.push({
+				        source: url,
+				        target: uri
+				    });
+		            process(uri);
+				    update();
+		        }
+	        });
+	    }else{
+	    	var item = JSON.parse(data);
+	    	var ln  =  nodes[url] || (nodes[url] = { name: url });
+	    		if(item['name']!=undefined){
+	    			ln.name = item['name'];
+	    		}else if(item['location']!=undefined){
+	    			ln.name = item['location'];
+	    		}
+	    		console.log(ln)
+	    		console.log("a");
+	    	var proc = function(x, k){
+	    		var uri = resolve(x.href, url);
+				    links.push({
+				        source: url,
+				        target: uri,
+				        rel:k
+				    });
+				    process(uri);
+
+	    	};
+	    	_.forEach(item['_links'],function(val, k){
+	    		if(_.isArray(val)){
+	    			_.forEach(val, function(x){
+	    				proc(x, k);
+	    			});
+	    		}else{
+	    			proc(val, k);
+	    		}
+	    	});
+			update();
+
+	    }
+    });
+}
+
+var links = [];
+
+
+// Compute the distinct nodes from the links.
+ 
+var width = 960,
+    height = 500;
+
+var force = d3.layout.force() 
+    .size([width, height])
+    .linkDistance(60)
+    .charge(-300)
+    .on("tick", tick) 
+
+var svg = d3.select(".view").append("svg")
+    .attr("width", width)
+    .attr("height", height);
+
+// Per-type markers, as they don't inherit styles.
+var defs = svg.append("defs")
+function marker(c){
+	var x = c.substring(1);
+	if($("#markers"+x).length>0){
+    	return "url(#markers"+x+")";
+	} 
+	defs.append("marker")
+	    .attr("id", "markers"+x)
+	    .attr("viewBox", "0 -5 10 10")
+	    .attr("refX", 15)
+	    .attr("refY", -1.5)
+	    .attr("markerWidth", 6)
+	    .attr("markerHeight", 6)
+	    .attr("orient", "auto")
+	    .append("path")
+	    .style('fill',c)
+	    .attr("d", "M0,-5L10,0L0,5");
+    return "url(#markers"+x+")";
+}
+var path = svg.append('g');
+var circles = svg.append('g');
+var text = svg.append('g');
+var current = null;
+1 
+var legends = d3.select('svg')
+    .append("g");
+function load(d){
+    $('.json').text("");
+    $.get(d.url, function(data) {
+    	$('.json').text(data);
+    	try{
+  			$(".json").JSONView(JSON.parse(data));
+  		}catch(e){
+
+  		}
+    });
+    current = d; 
+    update();
+}
+function update() { 
+    width = $(".view").width();
+    svg.attr("width", width);
+    link2 = _.map(links, function(l) {
+        var link = {}
+        link.type = l.rel||"initial";
+        link.source = nodes[l.source] || (nodes[l.source] = {
+            name: l.source,
+            url:l.source
+        });
+        link.target = nodes[l.target] || (nodes[l.target] = {
+            name: l.target,
+            url:l.target
+        });
+        return link;
+    });
+    force.size([width, height]).nodes(d3.values(nodes))
+        .links(link2).start();
+    path.selectAll("path")
+        .data(force.links())
+        .enter().append("path")
+        .attr("class", function(d) {
+            return "link " + d.type;
+        })
+        .style('stroke',function(d){return color(d.type);})
+        .attr("marker-end", function(d) {
+            return marker(color(d.type));
+        });
+    circles.selectAll("circle")
+        .attr("class",function(d){
+        	return current==d?"selected":"";
+        })
+        .data(force.nodes())
+        .enter().append("circle")
+        .attr("r", 6)
+        .on("click",load)
+        .style("stroke-width",function(d){
+        	return current==d?5:1;
+        })
+        .call(force.drag);
+    text.selectAll("text")
+        .data(force.nodes())
+        .text(function(d) {
+            return d.name;
+        })
+        .enter().append("text")
+        .attr("x", 8)
+        .attr("y", ".31em")
+        .text(function(d) {
+            return d.name;
+        });
+
+    force.start();
+    var legendRectSize = 20;
+    var legendSpacing = 5;
+    var legend = legends
+        .selectAll("g")
+        .data(color.domain())
+        .enter()
+        .append('g')
+          .attr('class', 'legend')
+          .attr('transform', function(d, i) {
+            var height = legendRectSize;
+            var x = 0;
+            var y = i * height;
+            return 'translate(' + x + ',' + y + ')';
+        });
+legend.append('rect')
+    .attr('width', legendRectSize)
+    .attr('height', legendRectSize)
+    .style('fill', color)
+    .style('stroke', color);
+ 
+legend.append('text')
+    .attr('x', legendRectSize + legendSpacing)
+    .attr('y', legendRectSize - legendSpacing)
+    .text(function(d) { return d; });
+}
+
+// Use elliptical arc path segments to doubly-encode directionality.
+function tick() {
+
+
+    path.selectAll("path").attr("d", linkArc);
+    circles.selectAll("circle").attr("transform", transform);
+    text.selectAll("text").attr("transform", transform);
+}
+
+function linkArc(d) {
+    var dx = d.target.x - d.source.x,
+        dy = d.target.y - d.source.y,
+        dr = Math.sqrt(dx * dx + dy * dy);
+    return "M" + d.source.x + "," + d.source.y + "A" + dr + "," + dr + " 0 0,1 " + d.target.x + "," + d.target.y;
+}
+
+function transform(d) {
+    return "translate(" + d.x + "," + d.y + ")";
+}
